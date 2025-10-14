6bf8b3edd7609703507113c3559db8e4fdab105d
pixel <srjuandi800@gmail.com>
2025-10-13 21:15:03 -0500

    cambios de configuracion

--- controllers/logica-email/envioClienteVeri.js
+++ controllers/logica-email/envioClienteVeri.js
@@ -2,18 +2,21 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 import dns from "dns";
 
-dns.setDefaultResultOrder("ipv4first"); // fuerza IPv4, evita errores EAI_AGAIN
+dns.setDefaultResultOrder("ipv4first");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // SSL
+  port: 587, // usa TLS
+  secure: false, // false para 587
+  requireTLS: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false, // evita fallos por certificados intermedios
+  },
 });
-console.log("MAIL_USER:", process.env.MAIL_USER);
-console.log("MAIL_PASSWORD:", process.env.MAIL_PASSWORD ? "OK" : "MISSING");
+
 export default transporter;
